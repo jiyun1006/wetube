13335ea0a1fb3f4e8a140817180f3c4e77627d61
jiyun1006 <kimjiyoon1006@hanmail.net>
2020-12-27 00:06:13 +0900

    edit file in db(before changing readme)

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,16 +1,23 @@
 import express from "express";
-import { deleteVideo, editVideo, getUpload, postUpload, videoDetail, videos } from "../controllers/videoController";
+import { deleteVideo, getEditVideo, postEditVideo, getUpload, postUpload, videoDetail } from "../controllers/videoController";
 import routes from "../routes";
-import {uploadVideo} from "../middlewares";
+import { uploadVideo } from "../middlewares";
 
 
 const videoRouter = express.Router();
 
+
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
 videoRouter.get(routes.deleteVideo, deleteVideo);
+
+
 
 
 export default videoRouter;
