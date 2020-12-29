cef5185afccc2e6f662f0bb76cd2b7e8b6c0365d
jiyun1006 <kimjiyoon1006@hanmail.net>
2020-12-30 00:15:26 +0900

    temporary css

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,18 +1,18 @@
 import routes from "./routes";
 import multer from "multer";
 
-const multerVideo = multer({dest:"uploads/videos/"})
+const multerVideo = multer({ dest: "uploads/videos/" })
 
-export const localsMiddleware =  (req, res, next) => {
+export const localsMiddleware = (req, res, next) => {
 
     res.locals.siteName = "WeTube";
     res.locals.routes = routes;
-    res.locals.user ={
-        isAuthenticated : true,
-        id : 1
+    res.locals.user = {
+        isAuthenticated: false,
+        id: 1
     }
     next();
 
 };
 
- export const uploadVideo = multerVideo.single("videoFile");
+export const uploadVideo = multerVideo.single("videoFile");
